96d714930c54e8817a38eee78ea69deef2eef19e
Xin Yu <66938425+chuahxinyu@users.noreply.github.com>
2021-11-20 15:12:41 +0800

    Display Programs and Add Search Functionality

--- src/pages/programs/index.js
+++ src/pages/programs/index.js
@@ -1,15 +1,24 @@
-import { Container, Input, Text, Flex, Grid, Button } from "@chakra-ui/react"
+import { Container, Input, Text, Flex, Grid, Box } from "@chakra-ui/react"
 import { SearchCard } from "../../components/Search/index"
+import axios from 'axios'
+import { useState, useEffect } from "react"
 
-import { useState } from "react"
 export default function Search() {
-  const [search_value, setSearchValue] = useState()
+  const [search_value, setSearchValue] = useState("")
   const handleChange = (event) => {
     setSearchValue(event.target.value)
   }
-  return (
+  const [searchCards, setSearchCards] = useState([])
 
-    <Container maxW={{ base: "", lg: "container.xl" }} mt={3}>
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/api/programs')
+      .then(response => {
+        setSearchCards(response.data)
+      })
+  }, [])
+  return (
+    <Box width="80%" margin="10px 10%">
       <Flex direction='row'>
         <Input
           value={search_value}
@@ -19,10 +28,27 @@ export default function Search() {
         />
       </Flex>
 
-      <Container>
-        <SearchCard />
-        <SearchCard />
-      </Container>
-    </Container>
+      <Grid 
+        alignItems="center"
+        templateColumns="repeat(auto-fill, minmax(300px, 1fr))"
+      >
+        {searchCards
+          .filter((val) => {
+            if (search_value == ""){
+              if (! searchCards.includes(val.id)) {
+                return val
+              }
+            } else if (val.name.toLowerCase().includes(search_value.toLowerCase())) {
+              return val
+            }
+            return null
+          })
+          .map((data, _) => {
+            return (
+              <SearchCard key={data.id} cardData={data}/>
+            )
+          })}
+      </Grid>
+    </Box>
   )
 }
