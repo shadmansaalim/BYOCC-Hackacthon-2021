edf58e9c690e94c4b6b35a310ddb8cd8b7c39f7f
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-11-20 13:53:06 +0600

    Bugs fixed amd spinner added to improve UX

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -5,6 +5,7 @@ import { DashboardCard } from "@components/Dashboard"
 import { useEffect } from "react"
 import { useState } from "react"
 import useAuth from "src/hooks/useAuth"
+import { CircularProgress } from '@chakra-ui/progress';
 
 export default function Dashboard() {
   const{user} = useAuth();
@@ -16,26 +17,41 @@ export default function Dashboard() {
   },[user.email])
   
   return (
-    <Box width="80%" margin="10px 10%">
-      <Flex direction="row">
-        <Text fontSize="2xl" style={{ fontWeight: 'bold' }}>My Loyalty Cards</Text>
-        <Spacer />
-        <Button leftIcon={<AddIcon />} colorScheme="green">Add</Button>
-      </Flex>
-      
-      <Grid 
-        alignItems="center"
-        templateColumns="repeat(auto-fill, minmax(300px, 1fr))"
-        my="8"
-      >
-      {
-        programs.map(program =>  <DashboardCard
-        key={program._id}
-        program={program}
-        ></DashboardCard>)
-      }
-      </Grid>
-    </Box>
+   <>
+   {
+     programs.length
+     ?
+     <Box width="80%" margin="10px 10%">
+     <Flex direction="row">
+       <Text fontSize="2xl" style={{ fontWeight: 'bold' }}>My Loyalty Cards</Text>
+       <Spacer />
+       <Button leftIcon={<AddIcon />} colorScheme="green">Add</Button>
+     </Flex>
+     
+     <Grid 
+       alignItems="center"
+       templateColumns="repeat(auto-fill, minmax(300px, 1fr))"
+       my="8"
+     >
+     {
+       programs.map(program =>  <DashboardCard
+       key={program._id}
+       program={program}
+       ></DashboardCard>)
+     }
+     </Grid>
+   </Box>
+   :
+   <div style={{
+    display: "flex",
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh'
+}}>
+    <CircularProgress isIndeterminate color="green.300" />
+</div>
+   }
+   </>
   )
 }
 
