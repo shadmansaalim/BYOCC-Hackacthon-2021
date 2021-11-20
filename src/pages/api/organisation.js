d1b4c39b997be4bc34b1d950c9ead299961cd353
Xin Yu <66938425+chuahxinyu@users.noreply.github.com>
2021-11-20 17:42:25 +0800

    Add Organisations Endpoint and GET function

---
+++ src/pages/api/organisation.js
@@ -0,0 +1,33 @@
+import { connectToDatabase } from "@lib/mongodb"
+
+export default async function handler (req, res) {
+  // switch the methods
+  switch (req.method) {
+    case 'GET': {
+      return getOrganisations(req, res)
+    }
+  }
+}
+
+async function getOrganisations(req, res) {
+  try {
+    // connect to database
+    let { db } = await connectToDatabase();
+    // fetch the organisations
+    const organisations = await db
+      .collection('organisations')
+      .find({})
+      .toArray()
+    // return the organisations
+    return res.json({
+        message: JSON.parse(JSON.stringify(organisations)),
+        success: true,
+    });
+  } catch (error) {
+    // return the error
+    return res.json({
+        message: new Error(error).message,
+        success: false,
+    });
+}
+}
