e00ca49afc9c6ae92de96b62735d0a7ba0976cdc
Magnuscake <saneyar.k@gmail.com>
2021-11-20 12:57:36 +1100

    refactor: clean up api endpoint for next js best practices

--- src/pages/api/users.js
+++ src/pages/api/users.js
@@ -1,66 +1,55 @@
-const { connectToDatabase } = require('../../lib/mogodb');
-const ObjectId = require('mongodb').ObjectId;
-
+import { connectToDatabase } from "@lib/mogodb"
 
 export default async function handler(req, res) {
-    // switch the methods
-    switch (req.method) {
+  // switch the methods
+  switch (req.method) {
+    case "POST":
+      await addUser(req, res)
+      break
 
-        case 'POST': {
-            return addUser(req, res);
-        }
+    case "PUT":
+      await updateUser(req, res)
+      break
 
-        case 'PUT': {
-            return updateUser(req, res);
-        }
-
-    }
+    default:
+      res.status(404).json({ success: false })
+  }
 }
 
 async function addUser(req, res) {
-    try {
-        console.log(req.body);
-        // connect to the database
-        let { db } = await connectToDatabase();
-        const usersCollection = db.collection("users");
-        const user = req.body;
-        console.log(user);
-        const result = await usersCollection.insertOne(user);
-        return res.json(result);
+  try {
+    console.log(req.body)
+    // connect to the database
+    let { db } = await connectToDatabase()
+    const usersCollection = db.collection("users")
+    const user = req.body
+    console.log(user)
 
-    }
+    const result = await usersCollection.insertOne(user)
 
-    catch (error) {
-        // return the error
-        return res.json({
-            message: new Error(error).message,
-            success: false,
-        });
-    }
+    res.status(200).json(result)
+  } catch (error) {
+    // return the error
+    res.sendStatus(error.status || 404).send("No user found")
+  }
 }
 async function updateUser(req, res) {
-    try {
-        console.log(req.body);
-        // connect to the database
-        let { db } = await connectToDatabase();
-        const usersCollection = db.collection("users");
-        const user = req.body;
-        const filter = { email: user.email };
-        const options = { upsert: true };
-        const updateDoc = {
-            $set: user
-        };
-        const result = await usersCollection.updateOne(filter, updateDoc, options);
-        res.json(result);
+  try {
+    console.log(req.body)
+    // connect to the database
+    let { db } = await connectToDatabase()
+    const usersCollection = db.collection("users")
+    const user = req.body
+    const filter = { email: user.email }
+    const options = { upsert: true }
+    const updateDoc = {
+      $set: user,
     }
+    const result = await usersCollection.updateOne(filter, updateDoc, options)
 
-    catch (error) {
-        // return the error
-        return res.json({
-            message: new Error(error).message,
-            success: false,
-        });
-    }
+    res.status(201).json(result)
+  } catch (error) {
+    // return the error
+    res.sendStatus(error.status).send("Error updating user")
+  }
 }
-
-
