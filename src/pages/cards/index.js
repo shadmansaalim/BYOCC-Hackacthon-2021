4c764b517dfbee35bfef283d5da87e8ad05aa1f7
Stephanie Renata <stephanierenata22@gmail.com>
2021-11-20 22:52:29 +1100

    card image layout fix

--- src/pages/cards/index.js
+++ src/pages/cards/index.js
@@ -1,7 +1,6 @@
 import {
   Text,
   Box,
-  Image,
   Container,
   Button,
   ButtonGroup,
@@ -11,9 +10,23 @@ import { StarIcon } from "@chakra-ui/icons"
 import { PlainCard } from "@components/LoyaltyCard"
 
 const cards = () => {
+  const business = {
+    name: "Starbucks",
+    descrip:
+      "Business details Lorem ipsum dolor sit amet, consectetur adipiscing elit Business details Lorem ipsum dolor sit amet, consectetur adipiscing elit Business details Lorem ipsum dolor sit amet",
+    avgRating: 4,
+    img: "https://bit.ly/2Z4KKcF",
+  }
+
   return (
     <div>
-      <Image src='https://bit.ly/2Z4KKcF' />
+      <Box
+        height='10em'
+        objectFit='cover'
+        backgroundImage={business.img}
+        backgroundPosition='center center'
+      />
+
       <Container mt={4} maxW={{ base: "", lg: "container.lg" }}>
         <Box
           my={4}
@@ -35,23 +48,43 @@ const cards = () => {
             <span>4.5</span>
           </Box>
         </Box>
-        <PlainCard />
 
-        <ButtonGroup
-          h='150px'
-          display='flex'
-          alignItems='center'
-          justifyContent='space-evenly'
-          flexDirection='column'
-          mt={9}
-        >
-          <Button h='45px' w='350px' borderRadius='40px' colorScheme='success'>
-            Buy Coffee
-          </Button>
-          <Button h='45px' w='350px' borderRadius='40px' colorScheme='danger'>
-            Remove Card
-          </Button>
-        </ButtonGroup>
+        <Box mt={5}>
+          <Box>
+            <PlainCard mx='auto' />
+          </Box>
+
+          <ButtonGroup
+            h='150px'
+            display='flex'
+            alignItems='center'
+            justifyContent='space-evenly'
+            flexDirection='column'
+            mt={9}
+          >
+            <Button
+              h='45px'
+              w='350px'
+              borderRadius='40px'
+              colorScheme='success'
+            >
+              Buy Coffee
+            </Button>
+            <Button h='45px' w='350px' borderRadius='40px' colorScheme='danger'>
+              Remove Card
+            </Button>
+          </ButtonGroup>
+          {/* <form>
+            <Text align='center'>User Unique Code:</Text>
+            <Text fontSize="5xl" textAlign='center'>{uniqueCode}</Text>
+            <Flex direction='row' mt={5} justify='center'>
+              
+              <Button width='400px' bg='#498741' color='white'>
+                Done
+              </Button>
+            </Flex>
+          </form> */}
+        </Box>
       </Container>
     </div>
   )
