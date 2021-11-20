dcdb34e3d0ca97f8115d8b56556db7c58768d3b5
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-11-20 22:00:20 +0600

    API updated

---
+++ src/pages/api/userData.js
@@ -0,0 +1,69 @@
+import { connectToDatabase } from "@lib/mongodb"
+
+export default async function handler(req, res) {
+  // switch the methods
+  switch (req.method) {
+    case "GET":
+      await getUserData(req, res)
+      break
+
+
+    case "PUT":
+      await updateUserData(req, res)
+      break
+
+    default:
+      res.status(404).json({ success: false })
+  }
+}
+
+async function getUserData(req, res) {
+  try {
+    console.log(req.body)
+    // connect to the database
+    let { db } = await connectToDatabase()
+    const usersCollection = db.collection("users")
+    const organisationCollection = db.collection("organisation")
+    const userEmail = req.query.email;
+    const query1 = { email: userEmail };
+    const user = await usersCollection.findOne(query1);
+    const userOrganisations = user.addedOrganisations;
+    const query2 = { organisationID: { $in: userOrganisations } };
+    const organisations = await organisationCollection.find(query2).toArray();
+    res.status(200).json(organisations)
+  } catch (error) {
+    // return the error
+    res.send("Could not add user to database")
+  }
+}
+async function updateUserData(req, res) {
+  try {
+    console.log(req.body)
+    // connect to the database
+    let { db } = await connectToDatabase()
+    const usersCollection = db.collection("users")
+    const organisationCollection = db.collection("organisation")
+    
+  } catch (error) {
+    // return the error
+    res.send("Could not add user to database")
+  }
+}
+
+
+
+// const users = [
+//   {
+//     email: "test123@gmail.com",
+//     displayName: "Test Account",
+//     addedOrganisations: ["o1", "o2"],
+//     numOfStamps: 0  
+//   },
+//   {
+//     email: "test2@gmail.com",
+//     displayName: "Test Account",
+//     addedOrganisations: ["o1"],
+//     numOfStamps: 0  
+//   }
+  
+// ]
--- src/pages/api/users.js
+++ src/pages/api/users.js
@@ -3,10 +3,6 @@ import { connectToDatabase } from "@lib/mongodb"
 export default async function handler(req, res) {
   // switch the methods
   switch (req.method) {
-    case "GET":
-      await getUserOrganisations(req, res)
-      break
-
     case "POST":
       await addUser(req, res)
       break
@@ -20,25 +16,7 @@ export default async function handler(req, res) {
   }
 }
 
-async function getUserOrganisations(req, res) {
-  try {
-    console.log(req.body)
-    // connect to the database
-    let { db } = await connectToDatabase()
-    const usersCollection = db.collection("users")
-    const organisationCollection = db.collection("organisation")
-    const userEmail = req.query.email;
-    const query1 = { email: userEmail };
-    const user = await usersCollection.findOne(query1);
-    const userOrganisations = user.addedOrganisations;
-    const query2 = { organisationID: { $in: userOrganisations } };
-    const organisations = await organisationCollection.find(query2).toArray();
-    res.status(200).json(organisations)
-  } catch (error) {
-    // return the error
-    res.send("Could not add user to database")
-  }
-}
+
 async function addUser(req, res) {
   try {
     console.log(req.body)
@@ -78,28 +56,3 @@ async function updateUser(req, res) {
 }
 
 
-// const users = [
-//   {
-//     email: "test123@gmail.com",
-//     displayName: "Test Account",
-//     addedOrganisations: ["o1", "o2"],
-//     numOfStamps: 0  
-//   },
-//   {
-//     email: "test2@gmail.com",
-//     displayName: "Test Account",
-//     addedOrganisations: ["o1"],
-//     numOfStamps: 0  
-//   }
-  
-// ]
-
-// USERS
-// - _id
-// - email
-// - displayName
-// - img
-// - addedPrograms:[{
-// - uniqueCode
-// - maxStamps
-// - numOfStamps
