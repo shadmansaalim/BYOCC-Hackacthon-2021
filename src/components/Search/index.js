9f36464d4031aa6bf9219551b31c1d942331b687
Stephanie Renata <stephanierenata22@gmail.com>
2021-11-20 12:52:02 +1100

    fix search page layout

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,5 +1,5 @@
-import { Box, Circle, Button } from "@chakra-ui/react";
-import { StarIcon } from "@chakra-ui/icons";
+import { Box, Circle, Button } from "@chakra-ui/react"
+import { StarIcon } from "@chakra-ui/icons"
 
 export const SearchCard = () => {
   const organisation = {
@@ -7,38 +7,40 @@ export const SearchCard = () => {
     imageUrl: "https://bit.ly/2Z4KKcF",
     reviews: [
       { text: "review1", rating: 4 },
-      { text: "review2", rating: 5 }
+      { text: "review2", rating: 5 },
     ],
-    avgRating: 4
-  };
+    avgRating: 4,
+  }
 
   return (
-    <Box as="button" minW="sm" borderWidth="1px" borderRadius="lg" m="3">
-      <Box
-        height="100px"
-        width="100%"
-        backgroundImage={organisation.imageUrl}
-        backgroundPosition="center center"
-      />
-      <Box p="3">
-        <Box display="flex" justifyContent="space-between">
-          <Box fontWeight="semibold" as="h4" lineHeight="tight" isTruncated>
-            {organisation.name}
-            <br />
-            <Box textAlign="left">
-              <StarIcon color="black" />
-              <Box as="span" ml={2}>
-                {organisation.avgRating.toFixed(1)}
+    <div>
+      <Box as='button' minW='sm' borderWidth='1px' borderRadius='lg' m='3'>
+        <Box
+          height='100px'
+          width='100%'
+          backgroundImage={organisation.imageUrl}
+          backgroundPosition='center center'
+        />
+        <Box p='3'>
+          <Box display='flex' justifyContent='space-between'>
+            <Box fontWeight='semibold' as='h4' lineHeight='tight' isTruncated>
+              {organisation.name}
+              <br />
+              <Box textAlign='left'>
+                <StarIcon color='black' />
+                <Box as='span' ml={2}>
+                  {organisation.avgRating.toFixed(1)}
+                </Box>
               </Box>
             </Box>
-          </Box>
-          <Box display="flex" alignItems="center">
-            <Button borderRadius="100" bg="green" color="white">
-              +
-            </Button>
+            <Box display='flex' alignItems='center'>
+              <Button borderRadius='100' bg='green' color='white'>
+                +
+              </Button>
+            </Box>
           </Box>
         </Box>
       </Box>
-    </Box>
-  );
-};
+    </div>
+  )
+}
--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,28 +1,29 @@
-import { Container, Input } from "@chakra-ui/react";
-import { SearchCard } from "../../components/Search";
+import { Container, Input } from "@chakra-ui/react"
+import { SearchCard } from "../../components/Search"
 
 export default function Search() {
   return (
     <div>
       <Container
-        height="1000px"
-        maxW="container.sm"
-        textAlign="center"
+        height='1000px'
+        maxW='container.sm'
+        textAlign='center'
         p={0}
         style={{
-          backgroundColor: "#F7F7F7"
+          backgroundColor: "#F7F7F7",
         }}
       >
-        <Container p={3} textAlign="left">
+        <Container p={3} textAlign='left'>
           <form>
-            <Input placeholder="Search" bg="white" />
+            <Input placeholder='Search' bg='white' />
           </form>
 
           <SearchCard />
-          <SearchCard />
-          <SearchCard />
+          <SearchCard/>
+          <SearchCard/>
+
         </Container>
       </Container>
     </div>
-  );
+  )
 }
