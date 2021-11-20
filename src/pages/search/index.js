ed670fa1ae4d2d6232dfe8dffb4c7e15c42ee3be
Stephanie Renata <stephanierenata22@gmail.com>
2021-11-20 15:42:14 +1100

    change add button to link

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,28 +1,28 @@
-import { Container, Input, Flex, Button } from "@chakra-ui/react"
-import { SearchCard } from "../../components/Search"
-import { useState } from 'react'
-export default function Search() {
-  const [search_value, setSearchValue] = useState();
-  const handleChange = (event) =>{
-    setSearchValue(event.target.value);
+import { Container, Input, Text, Flex, Grid, Button } from "@chakra-ui/react"
+import { SearchCard } from "../../components/Search/index"
+import { DashboardCard } from "../../components/Dashboard/index"
 
+import { useState } from "react"
+export default function Search() {
+  const [search_value, setSearchValue] = useState()
+  const handleChange = (event) => {
+    setSearchValue(event.target.value)
   }
   return (
-    <div>
-      <Container maxW={{ base: "", lg: "container.xl" }} mt={3}>
-      <Container textAlign='center'>
-      
-          <form>
-            <Input value={search_value} onChange={handleChange} placeholder='Search' bg='white'/>
-          </form>
-         
-          <SearchCard />
-          
+    <Container maxW={{ base: "", lg: "container.xl" }} mt={3}>
+      <Flex direction='row'>
+        <Input
+          value={search_value}
+          onChange={handleChange}
+          placeholder='Search'
+          bg='white'
+        />
+      </Flex>
 
-        </Container>
-
+      <Container>
+        <SearchCard />
+        <SearchCard />
       </Container>
-      
-    </div>
+    </Container>
   )
 }
