ffa3d960a29d71a89fa67ccd0bc9cde8f91048dd
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-11-27 16:37:31 +0600

    Updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
-import { AddIcon } from "@chakra-ui/icons"
-import { Button, Grid, Text, Box, Flex, Spacer, Link , Container} from "@chakra-ui/react"
-import { DashboardCard } from "@components/DashboardCard"
-import useAuth from "src/hooks/useAuth"
-import { CircularProgress } from "@chakra-ui/progress"
+import { AddIcon } from '@chakra-ui/icons'
+import { Button, Grid, Text, Box, Flex, Spacer, Link, Container } from '@chakra-ui/react'
+import { DashboardCard } from '@components/DashboardCard'
+import useAuth from 'src/hooks/useAuth'
+import { CircularProgress } from '@chakra-ui/progress'
 
-import PrivateRoute from "src/PrivateRoute/PrivateRoute"
-import { useEffect, useState } from "react"
-import { useRouter } from "next/router"
+import PrivateRoute from 'src/PrivateRoute/PrivateRoute'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
-export default function dashboard() {
+export default function dashboard () {
   const router = useRouter()
   const { user } = useAuth()
   const [programs, setPrograms] = useState([])
@@ -21,17 +21,18 @@ export default function dashboard() {
 
   return (
     <PrivateRoute>
-      {programs.length ? (
+      {programs.length
+        ? (
         <Container maxW="container.xl" margin='50px auto'>
           <Box>
-            <Text fontSize={{ base: "24px", md: "32px", lg: "42px" }} style={{ fontWeight: "bold" }}>
+            <Text fontSize={{ base: '24px', md: '32px', lg: '42px' }} style={{ fontWeight: 'bold' }}>
               My Loyalty Cards
             </Text>
           </Box>
 
           <Grid
           alignItems='center'
-          templateColumns={{sm: '1fr', md: 'repeat(2,1fr)',lg: 'repeat(3,1fr)',xl: 'repeat(3,1fr)'}}
+          templateColumns={{ sm: '1fr', md: 'repeat(2,1fr)', lg: 'repeat(3,1fr)', xl: 'repeat(3,1fr)' }}
           mt='8'
           columnGap={6}
           rowGap={3}
@@ -44,35 +45,38 @@ export default function dashboard() {
             ))}
           </Grid>
         </Container>
-      ) : (
+          )
+        : (
         <>
-          {programs === 0 ? (
+          {programs === 0
+            ? (
             <div
               style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                marginTop: "48px",
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                marginTop: '48px'
               }}
             >
-              <Text fontSize='xl' style={{ fontWeight: "bold" }}>
+              <Text fontSize='xl' style={{ fontWeight: 'bold' }}>
                 No programs added to Dashboard
               </Text>
             </div>
-          ) : (
+              )
+            : (
             <div
               style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "100vh",
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh'
               }}
             >
               <CircularProgress isIndeterminate color='green.300' />
             </div>
-          )}
+              )}
         </>
-      )}
+          )}
     </PrivateRoute>
   )
 }
